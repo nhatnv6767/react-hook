7c7f46a460abe4c33571d7d3553d30536f3263a7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-08 00:11:15 +0900

    #Render data - can not print/show object

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "None"
+  let number = 2022
+  let obj = { name: 'None', channel: 'Bu lang kkk' }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello World with React Hook</h1>
-        </p>
+        <h1>Hello World with React Hook - {name} in {obj}</h1>
       </header>
     </div>
   );

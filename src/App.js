1a8832c90952d5f992adcd13ab4946026ec15915
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-08 00:19:03 +0900

    #Render data - href link

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ function App() {
   let name = "None"
   let number = 2022
   let obj = { name: 'None', channel: 'Bu lang kkk' }
+  let link = 'https://github.com/nhatnv6767/react-hook'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World with React Hook - {name} in {obj.name}</h1>
+        <a href={link} target="_blank">Visit my channel</a>
       </header>
     </div>
   );
 }
+
+// <a href={link} target="_blank">Visit my channel</a> 
+// Sang 1 trang mới
 
 export default App;

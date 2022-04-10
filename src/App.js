a6fdd41b8a534288d884cc7908a01d944c2f1b5a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-11 00:13:42 +0900

    #16 useEffect Fetch Data : Tables

--- src/App.js
+++ src/App.js
@@ -60,7 +60,46 @@ function App() {
         <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World with React Hook - {name}</h1>
-        <Todo
+
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+          </tr>
+        </table>
+
+        {/* <Todo
           todos={todos}
           title={'All todos'}
           deleteDataTodo={deleteDataTodo}
@@ -72,7 +111,8 @@ function App() {
           deleteDataTodo={deleteDataTodo}
         />
         <input type="text" value={address} onChange={(event) => handleOnChangeInput(event)} />
-        <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
+        <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button> */}
+
       </header>
     </div>
   );

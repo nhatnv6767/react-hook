d3cb68da006b66979bb4a3fe7ffc41f1bc743327
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-10 22:22:39 +0900

    #13 Functions as Props

--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,17 +1,21 @@
 const Todo = (props) => {
     // property: sinh ra để truyền dữ liệu qua lại giữa các component
     // chỉ truyền dữ liệu từ cha xuống con, top => bottom
-    const todos = props.todos
+    const { todos, title } = props
     return (
         <div className='todos-container'>
             <div className='title'>
-                {props.title}
+                {title}
             </div>
             {todos.map(todo => {
                 return (
-                    <li className='todo-child' key={todo.id}>
-                        {todo.title}
-                    </li>
+                    <div key={todo.id}>
+                        <li className='todo-child' >
+                            {todo.title}
+                            <span> x</span>
+                        </li>
+
+                    </div>
                 )
             })}
             <hr />

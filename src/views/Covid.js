7e3b173820a947ba05c78a33a2ff72e245af5ec9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-11 01:09:35 +0900

    #16 useEffect Fetch Data : use Moment npm

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 
 const Covid = () => {
 
@@ -10,6 +11,17 @@ const Covid = () => {
         let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-01-01T00:00:00Z&to=2021-04-11T00:00:00Z')
         // if have response, and have res.data => res.data else []
         let data = res && res.data ? res.data : []
+        // sau khi lấy data về, ta check nếu có data và data.length
+        // sau đó sẽ map qua, lọc qua xuyên suốt cái mảng đó
+        // gán ngược lại cái biến Date của nó bằng moment.....
+        if (data && data.length > 0) {
+            data.map(item => {
+                item.Date = moment(item.Date).format('DD/MM/YYYY');
+                // cuối cùng bắt buộc phải return về cái item 
+                return item
+            })
+        }
+        console.log(">>>> CHECK DATE MOMENT", data);
         setDataCovid(data)
     }, []);
     return (
@@ -28,6 +40,7 @@ const Covid = () => {
                 {dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
                     return (
                         <tr key={item.ID}>
+                            <td>{item.Date}</td>
                             <td>{item.Confirmed}</td>
                             <td>{item.Active}</td>
                             <td>{item.Deaths}</td>

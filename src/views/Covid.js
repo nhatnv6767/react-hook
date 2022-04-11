4d2184622cb89a52eac5f1f5191f46601cb89d01
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-11 21:53:36 +0900

    #17 Conditional Loading Message

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -5,24 +5,29 @@ import moment from "moment";
 const Covid = () => {
 
     const [dataCovid, setDataCovid] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     // componentDidMount
     useEffect(async () => {
-        let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-01-01T00:00:00Z&to=2021-04-11T00:00:00Z')
-        // if have response, and have res.data => res.data else []
-        let data = res && res.data ? res.data : []
-        // sau khi lấy data về, ta check nếu có data và data.length
-        // sau đó sẽ map qua, lọc qua xuyên suốt cái mảng đó
-        // gán ngược lại cái biến Date của nó bằng moment.....
-        if (data && data.length > 0) {
-            data.map(item => {
-                item.Date = moment(item.Date).format('DD/MM/YYYY');
-                // cuối cùng bắt buộc phải return về cái item 
-                return item
-            })
-        }
-        console.log(">>>> CHECK DATE MOMENT", data);
-        setDataCovid(data)
+        setTimeout(async () => {
+            let res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-01-01T00:00:00Z&to=2021-04-11T00:00:00Z')
+            // if have response, and have res.data => res.data else []
+            let data = res && res.data ? res.data : []
+            // sau khi lấy data về, ta check nếu có data và data.length
+            // sau đó sẽ map qua, lọc qua xuyên suốt cái mảng đó
+            // gán ngược lại cái biến Date của nó bằng moment.....
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.Date = moment(item.Date).format('DD/MM/YYYY');
+                    // cuối cùng bắt buộc phải return về cái item 
+                    return item
+                })
+            }
+            data = data.reverse()
+            setDataCovid(data)
+            setLoading(false)
+        }, 3000)
+
     }, []);
     return (
         <>
@@ -38,17 +43,26 @@ const Covid = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataCovid && dataCovid.length > 0 && dataCovid.map(item => {
-                        return (
-                            <tr key={item.ID}>
-                                <td>{item.Date}</td>
-                                <td>{item.Confirmed}</td>
-                                <td>{item.Active}</td>
-                                <td>{item.Deaths}</td>
-                                <td>{item.Recovered}</td>
-                            </tr>
-                        )
-                    })}
+                    {loading === false && dataCovid && dataCovid.length > 0 &&
+                        dataCovid.map(item => {
+                            return (
+                                <tr key={item.ID}>
+                                    <td>{item.Date}</td>
+                                    <td>{item.Confirmed}</td>
+                                    <td>{item.Active}</td>
+                                    <td>{item.Deaths}</td>
+                                    <td>{item.Recovered}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    {loading === true
+                        && <tr>
+                            <td colSpan='5' style={{ 'textAlign': 'center' }}>
+                                Loading...
+                            </td>
+                        </tr>
+                    }
 
                 </tbody>
 

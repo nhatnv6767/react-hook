e812bab943ade78d82d4bd1026c4d3ab8f01962f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-12 23:08:19 +0900

    #19.1 Hook vs Class

---
+++ src/views/Countdown.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Countdown extends Component {
+    render() {
+        return (
+            <div>
+                Hello world with countdown
+            </div>
+        );
+    }
+}
+
+export default Countdown;

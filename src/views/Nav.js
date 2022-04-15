a258a3245a34b392bb95876bc456f7487199ac38
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-15 23:07:13 +0900

    #20 React Router navigation 1

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -5,9 +5,9 @@ function Nav() {
     return (
         <div className="topnav">
             <a className="active" href="#home">Home</a>
-            <a href="#news">Timer Apps</a>
-            <a href="#contact">Todo Apps</a>
-            <a href="#about">Secret</a>
+            <a href="/timer">Timer Apps</a>
+            <a href="/todo">Todo Apps</a>
+            <a href="/secret">Secret</a>
         </div>
     )
 }

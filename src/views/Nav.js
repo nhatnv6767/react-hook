581f21e47ece4a69407cac2fa092c6bfe22d5805
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-16 23:12:51 +0900

    #21 Moẻ React Router - class active

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Nav.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Nav() {
     return (
@@ -15,5 +15,7 @@ function Nav() {
 
 // khi dùng Link nó sẽ cancel request gửi lên server
 // tức là hành động mặc định của thẻ a sẽ gửi 1 request lên phía server
+
+// NavLink để set cái class active vào
 
 export default Nav

50b1ff4a1c4878aaaad151b1d62a15c1661d3ca8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-17 23:30:47 +0900

    #23 Route Parameters: Detail blog

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Todo from './views/Todo'
 import Covid from './views/Covid'
 import { Countdown, NewCountDown } from './views/Countdown'
 import Blog from './views/Blog'
+import DetailBlog from './views/DetailBlog'
 import {
   BrowserRouter as Router,
   Switch,
@@ -96,8 +97,11 @@ function App() {
             <input type="text" value={address} onChange={(event) => handleOnChangeInput(event)} />
             <button type="button" onClick={(event) => handleEventClick(event)}>Click me</button>
           </Route>
-          <Route path="/blog">
+          <Route path="/blog" exact>
             <Blog />
+          </Route>
+          <Route path="/blog/:id">
+            <DetailBlog />
           </Route>
         </Switch>
       </div>

0de21df08df10414c283275578ab9d20a2e100aa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-18 00:37:54 +0900

    #26 controlled inputs forms NEW FORM

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function AddNewBlog() {
     return (
-        <div>AddNewBlog</div>
+        <div>
+            <div>
+                <label>Title: </label>
+                <input type="text" />
+            </div>
+            <div>
+                <label>Content: </label>
+                <input type="text" />
+            </div>
+            <button>Submit</button>
+        </div>
     )
 }
 

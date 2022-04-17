cf48c538a9b0f10c68e5531725b52e9a644dfa22
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-17 22:43:04 +0900

    #23 Route Parameters: fetch blog and explain

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   // will run after first render
   // will run when re-render
   useEffect(() => {
-    console.log('>>>> Run use effect address')
+    // console.log('>>>> Run use effect address')
     // việc thêm [] <dependencies> nghĩa là hàm useEffect này chỉ chạy duy nhất 
     // 1 lần <=> ComponentDidmount
     // chạy 1 lần duy nhất sau khi render lần đầu
@@ -39,7 +39,7 @@ function App() {
 
 
   useEffect(() => {
-    console.log('>>>> Run use effect todos')
+    // console.log('>>>> Run use effect todos')
   }, [todos]);
 
 
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import useFetch from '../customize/fetch'
 
 function Blog() {
+
+    const { data: dataBlogs, isLoading, isError } =
+        useFetch(`https://jsonplaceholder.typicode.com/posts`, false);
+
+    console.log('Check DATA BLOG: ', dataBlogs);
+
     return (
         <div>Blog</div>
     )
 }
 
 export default Blog
+
+// lần đầu khi chạy log ra 3 lần, vì ở fetch custom có 3 tham số
+// ở fetch chúng ta set 3 lần (setData, setIsLoading, setIsError), 3 lần set lại state
+// nên component của ta cần render lại 3 lần

80d4290d0bbb22ac02e18ca89043d4e254632758
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-17 23:50:32 +0900

    #24 Forward page Route history

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -3,9 +3,18 @@ import { useParams, useHistory } from 'react-router-dom'
 
 function DetailBlog() {
     let { id } = useParams();
+    let history = useHistory();
+
+    const handleBackData = () => {
+        history.push("/blog")
+    }
     return (
         <>
-            <div>&#60;-- Back</div>
+            <div>
+                <span style={{ cursor: 'pointer' }} onClick={handleBackData}>
+                    &#60;-- Back
+                </span>
+            </div>
             <h1>DetailBlog with id: {id}</h1>
         </>
     )

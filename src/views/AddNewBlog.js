9a5ea8aeb50a4feb8aae24d567d3a1eac95a41f4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-18 00:40:17 +0900

    #26 controlled inputs forms scss

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './Blog.scss'
 
 function AddNewBlog() {
     return (
-        <div>
+        <div className="add-new-container">
+            <div className="text-add-new">--- Add new blog ---</div>
             <div>
                 <label>Title: </label>
                 <input type="text" />

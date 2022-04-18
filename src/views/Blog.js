936218d3d98be94e17a8b141118e8b981113debc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-18 23:47:26 +0900

    #28 ... Modal react bootstrap 2

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -5,6 +5,11 @@ import { Link, useHistory } from 'react-router-dom'
 import Modal from 'react-bootstrap/Modal'
 
 function Blog() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     let history = useHistory();
     const { data: dataBlogs, isLoading, isError } =
         useFetch(`https://jsonplaceholder.typicode.com/posts`, false);
@@ -21,6 +26,30 @@ function Blog() {
 
     return (
         <>
+
+
+            <>
+                <Button variant="primary" onClick={handleShow}>
+                    Launch demo modal
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+
+
             <div>
                 <button
                     className="btn-add-new"
@@ -58,34 +87,3 @@ export default Blog
 // nên component của ta cần render lại 3 lần
 
 
-function Example() {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
-}
-
-render(<Example />);

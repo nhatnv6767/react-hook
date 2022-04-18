24174fbe1cd1c26a770b6278394feea4e33bcca7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-19 01:04:08 +0900

    #29 useHistory to Back to Home Page

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function NotFound() {
+  let history = useHistory()
+  const handleClickBtn = () => {
+    history.push('/')
+  }
   return (
     <div className="not-found-container">
       <h4>This page isn't available</h4>
       <h5>The link may be broken, or the page may have been removed. Check to see if the link you're trying to open is correct.</h5>
-      <button className="btn btn-primary">Go to HomePage</button>
+      <button
+        className="btn btn-primary"
+        onClick={handleClickBtn}
+      >
+        Go to HomePage
+      </button>
     </div>
   )
 }

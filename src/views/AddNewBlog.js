ad9f05e43d744d480bd12e58ebd72931ff39b7dc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-18 21:26:01 +0900

    #26 controlled inputs forms ___ useState

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Blog.scss'
+import { useState } from 'react'
 
 function AddNewBlog() {
+    // state
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
     return (
         <div className="add-new-container">
             <div className="text-add-new">--- Add new blog ---</div>
             <div className="inputs-data">
                 <label>Title: </label>
-                <input type="text" />
+                <input type="text" value={title} />
             </div>
             <div className="inputs-data">
                 <label>Content: </label>

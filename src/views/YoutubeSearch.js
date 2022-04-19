25c1bb55bc2e6ae275e757f198749fd2c898c331
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-19 23:29:50 +0900

    #30.2 Search Youtube 6

--- src/views/YoutubeSearch.js
+++ src/views/YoutubeSearch.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import './Blog.scss';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 function YoutubeSearch() {
+
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+
+        // component vừa mount xong thì sẽ gọi useEffect nếu để dependence là []
+    }, [])
+
     return (
         <div className="youtube-search-container">
             <div className="yt-search">
